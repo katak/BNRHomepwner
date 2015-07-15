0ef87ebd697aa8a725d68f0d0f86651fd01b5ca0
Kris Kata <kata.krzysztof@gmail.com>
2015-07-14 19:24:41 -0700

    added BNRItem class from previous project

---
+++ Homepwner/BNRItem.h
@@ -0,0 +1,27 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by Kris Kata on 5/9/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+
+@property (nonatomic, copy) NSString *itemName;
+@property (nonatomic, copy) NSString *serialNumber;
+@property (nonatomic) int valueInDollars;
+@property (nonatomic, readonly, strong) NSDate *dateCreated;
+
++ (instancetype)randomItem;
+
+// Designated initializer for BNRItem
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+
+@end
