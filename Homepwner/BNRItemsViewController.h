afbabf711fa0ba8b872e68ecb92b4b661308f143
Kris Kata <kata.krzysztof@gmail.com>
2015-07-14 19:12:57 -0700

    Created custom table view controller

---
+++ Homepwner/BNRItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRItemsViewController.h
+//  Homepwner
+//
+//  Created by Kris Kata on 7/14/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRItemsViewController : UITableViewController
+
+@end
---
+++ Homepwner/BNRItemsViewController.m
@@ -0,0 +1,25 @@
+//
+//  BNRItemsViewController.m
+//  Homepwner
+//
+//  Created by Kris Kata on 7/14/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRItemsViewController.h"
+
+@implementation BNRItemsViewController
+
+- (instancetype)init
+{
+    // Call the superclass's designated initializer
+    self = [super initWithStyle:UITableViewStylePlain];
+    return self;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+@end
