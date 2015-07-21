a2e5d76fa59564152741d5ab8da5ebda8803ec1a
Kris Kata <kata.krzysztof@gmail.com>
2015-07-20 19:24:14 -0700

    added view for item details

---
+++ Homepwner/BNRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRDetailViewController.h
+//  Homepwner
+//
+//  Created by Kris Kata on 7/20/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRDetailViewController : UIViewController
+
+@end
---
+++ Homepwner/BNRDetailViewController.m
@@ -0,0 +1,24 @@
+//
+//  BNRDetailViewController.m
+//  Homepwner
+//
+//  Created by Kris Kata on 7/20/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRDetailViewController.h"
+
+@interface BNRDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UILabel *dateField;
+
+@end
+
+@implementation BNRDetailViewController
+
+
+
+@end
